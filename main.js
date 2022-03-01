a15adae986fbe569d22a1621bf7f187ff7215ac5
Naozumi <naozumi@naozumi.me>
2022-03-01 11:33:01 +0800

    Fix image format

--- main.js
+++ main.js
@@ -14,11 +14,11 @@ const main = async () => {
     const pages = await browser.pages();
     const page = pages[0];
     page.setUserAgent('Chrome');
-    await page.goto('https://discord.com/channels/' + config.serverID + '/' + config.channelID + '/0', { waitUntil: 'networkidle0', timeout: 0 });
+    await page.goto('https://discord.com/channels/' + config.serverID + '/' + config.channelID + '/0', { waitUntil: 'networkidle0' });
     page.on("request", async req => {
         if (req.url().startsWith('https://media.discordapp.net/attachments/') && req.resourceType() === 'image') { //https://media.discordapp.net/attachments/123456789012345678/876543210987654321/IMG_7448.jpg?width=1018&height=1357
-            const imageUrl = req.url().split('?width=')[0];
-            const imageName = path.basename(imageUrl);
+            const imageUrl = req.url().split(/\?format=|\?width=/g)[0];
+            const imageName = path.win32.basename(imageUrl);
             download(imageUrl, `./spider/${imageName}`, function (state) {
                 console.log(`Downloading: ${imageUrl}`);
                 if (config.showDownloadDetails) console.log("progress", state);
@@ -32,7 +32,7 @@ const main = async () => {
         }
     });
     const scrollable_section = '#app-mount > div.app-3xd6d0 > div > div.layers-OrUESM.layers-1YQhyW > div > div > div > div > div.chat-2ZfjoI > div.content-1jQy2l > main > div.messagesWrapper-RpOMA3.group-spacing-16 > div';
-    await page.waitForSelector(scrollable_section);
+    await page.waitForSelector(scrollable_section, { timeout: 0 });
     setInterval(() => {
         page.evaluate(selector => {
             const scrollableSection = document.querySelector(selector);
